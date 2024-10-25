ea5533f97c65d3118a1895ec30dfbc5571d01e69
Nicolas Delfino <delfinonicolas15@gmail.com>
2024-10-25 08:46:38 -0300

    Update Windows Cleaner.bat

--- Windows Cleaner.bat
+++ Windows Cleaner.bat
@@ -1,51 +1,51 @@
-rem This script was written by Nicolas Delfino - Published in 2024
-rem email: delfinonicolas15@gmail.com
+rem Este script foi escrito por Nicolas Delfino - Publicado em 2024
+rem e-mail: delfinonicolas15@gmail.com
 cls
 
 @echo off
 
-rem Check If User Has Admin Privileges
+rem Verifica se o usuário tem privilégios de administrador
 timeout /t 1 /nobreak > NUL
 openfiles > NUL 2>&1
 if %errorlevel%==0 (
     echo Running..
 ) else (
-    echo You must run me as an Administrator. Exiting..
+    echo Você deve executar este script como Administrador. Saindo..
     echo.
-    echo Right-click on me and select ^'Run as Administrator^' and try again.
+    echo Clique com o botão direito sobre mim e selecione ^'Executar como Administrador^' e tente novamente.
     echo.
-    echo Press any key to exit..
+    echo Pressione qualquer tecla para sair..
     pause > NUL
     exit
 )
 
 echo.
 
-rem Delete Temporary Files
+rem Deletar arquivos temporários
 del /s /f /q %WinDir%\Temp\*.*
 del /s /f /q %WinDir%\Prefetch\*.*
 del /s /f /q %Temp%\*.*
 del /s /f /q %AppData%\Temp\*.*
 del /s /f /q %HomePath%\AppData\LocalLow\Temp\*.*
 
-rem Delete Used Drivers Files (Not needed because already installed)
+rem Deletar arquivos de drivers usados (não necessário, pois já estão instalados)
 del /s /f /q %SYSTEMDRIVE%\AMD\*.*
 del /s /f /q %SYSTEMDRIVE%\NVIDIA\*.*
 del /s /f /q %SYSTEMDRIVE%\INTEL\*.*
 
-rem Delete Temporary Folders
+rem Deletar pastas temporárias
 rd /s /q %WinDir%\Temp
 rd /s /q %WinDir%\Prefetch
 rd /s /q %Temp%
 rd /s /q %AppData%\Temp
 rd /s /q %HomePath%\AppData\LocalLow\Temp
 
-rem Delete Used Drivers Folders (Not needed because already installed)
+rem Deletar pastas de drivers usados (não necessário, pois já estão instaladas)
 rd /s /q %SYSTEMDRIVE%\AMD
 rd /s /q %SYSTEMDRIVE%\NVIDIA
 rd /s /q %SYSTEMDRIVE%\INTEL
 
-rem Recreate Empty Temporary Folders
+rem Recriar pastas temporárias vazias
 md %WinDir%\Temp
 md %WinDir%\Prefetch
 md %Temp%
@@ -53,7 +53,7 @@ md %AppData%\Temp
 md %HomePath%\AppData\LocalLow\Temp
 
 echo.
-echo Windows Clean Up Done!, You can exit by pressing any key.
+echo Limpeza do Windows concluída! Você pode sair pressionando qualquer tecla.
 echo.
 
 pause > NUL
